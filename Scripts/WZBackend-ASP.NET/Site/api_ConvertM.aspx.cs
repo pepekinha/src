0e3ea19dfb3dcfdf53054fd9e266d9736e57ecee
ViruZ Server <Leopinheirohn@gmail.com>
2014-03-07 04:25:11 -0300

    Scripts

---
+++ Scripts/WZBackend-ASP.NET/Site/api_ConvertM.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Data;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class api_SrvSkills : WOApiWebPage
+{
+    void OutfitOp( string Var1,string Var2,string CustomerID)
+    {
+
+        SqlCommand sqcmd = new SqlCommand();
+        sqcmd.CommandType = CommandType.StoredProcedure;
+        sqcmd.CommandText = "WZ_ConvertGD";
+        sqcmd.Parameters.AddWithValue("@in_CustomerID", CustomerID);
+        sqcmd.Parameters.AddWithValue("@in_Var1", Var1);
+        sqcmd.Parameters.AddWithValue("@in_Var2", Var2);
+
+        if (!CallWOApi(sqcmd))
+            return;
+
+        Response.Write("WO_0");
+    }
+
+    protected override void Execute()
+    {
+        if (!WoCheckLoginSession())
+            return;
+  string CustomerID = web.Param("CustomerID");
+       string Var1 = web.Param("Var1");
+        string Var2 = web.Param("Var2");
+
+        OutfitOp(Var1,Var2,CustomerID);
+    }
+}
---
+++ Scripts/WZBackend-ASP.NET/Site/api_lobuginek.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Data;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class api_Login : WOApiWebPage
+{
+    protected override void Execute()
+    {
+        string username = web.Param("username");
+        string password = web.Param("password");
+
+        SqlCommand sqcmd = new SqlCommand();
+        sqcmd.CommandType = CommandType.StoredProcedure;
+        sqcmd.CommandText = "WZ_ACCOUNT_LOGIN";
+        sqcmd.Parameters.AddWithValue("@in_IP", LastIP);
+        sqcmd.Parameters.AddWithValue("@in_EMail", username);
+        sqcmd.Parameters.AddWithValue("@in_Password", password);
+
+        if (!CallWOApi(sqcmd))
+            return;
+
+        reader.Read();
+        int CustomerID = getInt("CustomerID"); ;
+        int AccountStatus = getInt("AccountStatus");
+        int SessionID = 0;
+        int IsDeveloper = 0;
+
+        if (CustomerID > 0)
+        {
+            SessionID = getInt("SessionID");
+            IsDeveloper = getInt("IsDeveloper");
+        }
+
+        GResponse.Write("WO_0");
+        GResponse.Write(string.Format("{0} {1} {2}",
+            CustomerID, SessionID, AccountStatus));
+    }
+}
